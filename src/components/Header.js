19dd28435f60c1a36020283cb5bc348f06f4bb4d
ripal-designer <55450429+ripal-designer@users.noreply.github.com>
2022-05-06 11:59:47 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,20 +7,20 @@ function Header() {
     <div className=''>
 <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="">Navbar</a>
+    <a className="navbar-brand" href={null}>Navbar</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href={null}>Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href={null}>Link</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+          <a className="nav-link disabled" href={null} tabIndex="-1" aria-disabled="true">Disabled</a>
         </li>
       </ul>
       <form className="d-flex" >
