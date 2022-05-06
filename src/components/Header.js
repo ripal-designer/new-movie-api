68a36f52b38340e2b46ad2c8d3c8eacd3812adab
ripal-designer <55450429+ripal-designer@users.noreply.github.com>
2022-05-06 12:08:05 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,20 +7,20 @@ function Header() {
     <div className=''>
 <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="home" >Navbar</a>
+    <button className="navbar-brand"  >Navbar</button>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" >Home</a>
+          <button  className="nav-link active" aria-current="page" >Home</button>
         </li>
         <li className="nav-item">
-          <a className="nav-link" >Link</a>
+          <button className="nav-link" >Link</button>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled"  tabIndex="-1" aria-disabled="true">Disabled</a>
+          <button className="nav-link disabled"  tabIndex="-1" aria-disabled="true">Disabled</button>
         </li>
       </ul>
       <form className="d-flex" >
