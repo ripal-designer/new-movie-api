3dc0f1c6b0c1fddd7819522dd072b7cb0b558c2e
ripal-designer <55450429+ripal-designer@users.noreply.github.com>
2022-05-06 11:55:35 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
     <div className=''>
 <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="">Navbar</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
