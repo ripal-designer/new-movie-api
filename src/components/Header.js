5398ab279e71c27e496c5b85b0a386fdab26e074
ripal-designer <55450429+ripal-designer@users.noreply.github.com>
2022-05-06 12:09:33 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ function Header() {
         </li>
       </ul>
       <form className="d-flex" >
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" aria-live='searchFormInput' />
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
         <button className="btn btn-outline-light" type="submit" >Search</button>
       </form>
     </div>
